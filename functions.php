bd4d52fd0ac37b4ca2956ad96365d21959d0bd3a
Ümit Özay <umitozay95@gmail.com>
2025-03-26 16:13:53 +0300

    functions.php

--- functions.php
+++ functions.php
@@ -2,3 +2,12 @@
 
 require_once get_theme_file_path() . '/wp-bootstrap-navwalker.php';
 
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'primary' => __( 'Ust Menu', 'umitweb' ),
+        )
+    );
+}
+add_action( 'init', 'register_my_menus' );
+
